eddee19f8b9c6b8af752139cff92f49115b65e26
khaled Reza <khaledreza@gmail.com>
2023-08-14 16:13:33 +0200

    removed ssl certificates

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: "./restaurentapp2023-privateKey.key",
-      cert: "./restaurentapp2023.crt",
-    },
-  },
   plugins: [react()],
 });
