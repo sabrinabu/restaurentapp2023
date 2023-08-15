920bb20178332318605c3e904912d4edbf5eb532
khaled Reza <khaledreza@gmail.com>
2023-08-15 19:30:41 +0200

    added server configuration and auth0 library again

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /*server: {
+    https: {
+      key: "",
+      cert: "",
+    },
+  },*/
   plugins: [react()],
   build: {
     outDir: "build",
